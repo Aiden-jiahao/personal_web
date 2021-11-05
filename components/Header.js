71371f44c4000c6df48f18b8a1374215d182b814
Aiden <aidenjiahao@gmail.com>
2021-11-04 21:23:53 -0400

    no message

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
-
 function Header() {
   return (
     <div className=" flex items-center md:border-2  rounded-full py-2  md:shadow-sm space-x-4 ">
-      <h1> Project</h1>
-      <h1> Contact Me</h1>
-      <h1> Resume</h1>
+      <h1 className="text-3xl"> Project</h1>
+      <h1 className="text-3xl"> Contact Me</h1>
+      <h1 className="text-3xl text-red-700 "> Resume</h1>
     </div>
   );
 }
