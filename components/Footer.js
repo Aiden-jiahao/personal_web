0af2214d2d4629b78640bf77ea2617e694092a5f
Aiden <aidenjiahao@gmail.com>
2021-11-06 19:07:04 -0400

    no message

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,23 @@
 import React from "react";
-
+import ReactPlayer from "react-player";
 function Footer() {
   return (
     <div>
       <p>this is the footer </p>
+      <div className="px-20 py-10">
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=JxjD0ez0zEg"
+          controls
+          playbackRate={1}
+          width="700px"
+          height="400px"
+
+          /* controls = Display native player controls 
+            playbackRate = Set the playback rate of the player
+            width = Set the width of the player
+            height = Set the height of the player*/
+        />
+      </div>
     </div>
   );
 }
