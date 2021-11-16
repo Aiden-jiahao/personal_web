e5338f97e7b0d8f3908235e2cf887a644cb2f39a
Aiden <aidenjiahao@gmail.com>
2021-11-16 18:52:42 -0500

    no message

--- components/Contact.js
+++ components/Contact.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 import emailjs from "emailjs-com";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 const Contact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -27,35 +31,56 @@ const Contact = () => {
       setMessage("");
       setEmailSent(true);
     } else {
-      alert("Please fill in all fields.");
+      alert("Please fill in all fields. Thanks !");
     }
   };
 
   return (
-    <div id="contact-form">
-      <input
-        type="text"
-        placeholder="Your Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="email"
-        placeholder="Your email address"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <textarea
-        placeholder="Your message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      ></textarea>
-      <button onClick={submit} className="hover:scale-110">
-        Send Message
-      </button>
-      <span className={emailSent ? "visible" : null}>
-        Thank you for your message, we will be in touch in no time!
-      </span>
+    <div>
+      <Header />
+      <div className="relative z-10 px-5 lg:max-w-6xl mx-auto -mt-4">
+        <h1 className=" font-semibold text-2xl">Lets get in touch!</h1>
+        <span className="font-light text-base mt-2">
+          Fill in the details and I will get back to you ASAP 🚀
+        </span>
+
+        <form className="form rounded-lg flex flex-col gap-y-4 pt-4 max-w-full">
+          <TextField
+            type="text"
+            placeholder="Your Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="dark: bg-gray-100"
+          />
+
+          <TextField
+            type="email"
+            placeholder="Your email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="dark: bg-gray-100"
+          />
+          <TextField
+            placeholder="Your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className="dark: bg-gray-100"
+            multiline
+            rows={8}
+            maxRows={10}
+          />
+          <Button
+            onClick={submit}
+            variant="contained"
+            size="small"
+            className="hover:110"
+          >
+            Send Message
+          </Button>
+        </form>
+      </div>
+
+      <Footer />
     </div>
   );
 };
